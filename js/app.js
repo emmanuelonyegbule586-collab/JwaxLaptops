41ad743be4d496287619e48b01f586b6d28a6122
emmanuelonyegbule586-collab <emmanuelonyegbule586@gmail.com>
2026-06-10 08:31:25 -0500

    Fix mobile menu

--- js/app.js
+++ js/app.js
@@ -212,3 +212,9 @@ document.addEventListener("DOMContentLoaded", () => {
         AppState.toggleThemeMode();
     });
 });
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
